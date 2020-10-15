037e2a92d4e171b2eda8b0095d562f50cb6b3717
jaskaransing108373 <jaskaransing108373@github.com>
2020-10-15 05:45:21 -0400

    addd

--- js/modules/DataModule.js
+++ js/modules/DataModule.js
@@ -1,5 +1,22 @@
 export default {
-    name: "jas",
-    role: "student",
-    nickname: "jass"
+    Justin: {
+        name: "jas",
+        role: "student",
+        nickname: "jass"  
+    },
+    John: {
+        name: "john",
+        role: "super chill",
+        nickname: "just john"
+    },
+    Joe: {
+        name: "joe",
+        role: "prof",
+        nickname: "teddy bear"
+    },
+    Pan: {
+        name: "pan",
+        role: "code Ninja",
+        nickname: "Spider Pan"
+    },
 }
